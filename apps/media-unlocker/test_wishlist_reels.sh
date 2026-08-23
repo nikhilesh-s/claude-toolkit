a865beb3125e2170b6543d52a1ea6366ac7c3441
Nikhilesh Suravarjjala <123913414+nikhilesh-s@users.noreply.github.com>
2026-08-23 14:06:56 -0700

    Add latest wishlist reels to regression set

--- apps/media-unlocker/test_wishlist_reels.sh
+++ apps/media-unlocker/test_wishlist_reels.sh
@@ -12,6 +12,8 @@ URLS=(
   'https://www.instagram.com/reel/Da-PFaqJBOZ/?igsi=NTc4MTIwNjQ2YQ=='
   'https://www.instagram.com/reel/DZGtSP6x4vc/?igsi=NTc4MTIwNjQ2YQ=='
   'https://www.instagram.com/reel/Db8ewR9uoJN/?igsi=NTc4MTIwNjQ2YQ=='
+  'https://www.instagram.com/reel/DaMbGNOTsQn/?igsi=NTc4MTIwNjQ2YQ=='
+  'https://www.instagram.com/reel/DYVbQQ1xC_6/?igsi=NTc4MTIwNjQ2YQ=='
 )
 
 printf 'Testing wishlist Instagram reels with browser cookies: %s\n\n' "$BROWSER"
